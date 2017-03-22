6ebe1e4809afafc7e41acec3c0abdba764760594
loveKino <842914439@qq.com>
2017-03-22 19:00:28 +0800

    node name

--- example/button/assets/app.js
+++ example/button/assets/app.js
@@ -11640,7 +11640,7 @@ module.exports = {
 
 
 module.exports = (node) => {
-    let tagName = node.nodeName.toLowerCase();
+    let tagName = node.nodeName && node.nodeName.toLowerCase();
     if (tagName === 'input' || tagName === 'textarea') {
         return node.getAttribute('value');
     }
--- src/restraint/contentExtractor/inputValue.js
+++ src/restraint/contentExtractor/inputValue.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = (node) => {
-    let tagName = node.nodeName.toLowerCase();
+    let tagName = node.nodeName && node.nodeName.toLowerCase();
     if (tagName === 'input' || tagName === 'textarea') {
         return node.getAttribute('value');
     }
